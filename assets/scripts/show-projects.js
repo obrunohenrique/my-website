fa417045da6975e3650bd02c7800ab9adf6531a8
Bruno Henrique <brunohenriquef.pinto@gmail.com>
2024-10-21 13:43:44 -0300

    update

--- assets/scripts/show-projects.js
+++ assets/scripts/show-projects.js
@@ -5,6 +5,27 @@ const projects = [
         descricao: "O que deveria ser uma calculadora simples se tornou um website que converte moedas em tempo real e é capaz de retornar previsões de investimentos de renda fixa.",
         acesso: "https://github.com/obrunohenrique/Calcule.io",
         repositorio: "https://github.com/obrunohenrique/Calcule.io"
+    }, 
+    {
+        icon: "medication",
+        title: "Lufarma",
+        descricao: "o projeto final do módulo 1 na Formação Acelerada de Programação da Softex Pernambuco. Uma aplicação em Javascript para consolidar os conhecimentos em lógica de programação.",
+        acesso: "https://www.linkedin.com/feed/update/urn:li:activity:7224956415839248386/",
+        repositorio: "https://github.com/LucemilaBonifacio/LuFarma"
+    },
+    {
+        title: "MicroProcessador MIPS",
+        icon: "developer_board",
+        descricao: "Projeto desenvolvido para a disciplina de sistemas digitais - implementação em verilog de uma microarquitetura MIPS de ciclo único, capaz de rodar um software também desenvolvido pela equipe.",
+        acesso: "https://www.linkedin.com/feed/update/urn:li:activity:7229472175009734656/",
+        repositorio: "https://github.com/lucasmoraismt/MIPS_microprocessor"
+    },
+    {
+        icon: 'joystick',
+        title: "Don't touch me",
+        descricao: "um jogo 'one hit kill' que tem como objetivo coletar o maior número de moedas sem ser tocado pelos obstáculos. A diversão do jogo reside na dificuldade de desviar dos obstáculos enquanto se coleta as moedas espalhadas pela tela.",
+        acesso: "https://www.linkedin.com/feed/update/urn:li:activity:7221645872499957760/",
+        repositorio: "https://github.com/obrunohenrique/don-t-touch-me"
     }
 ]
 
@@ -20,34 +41,40 @@ for (let n = 0; n <= projects.length; n++) {
     const span = document.createElement("span")
     span.classList.add("material-symbols-outlined")
     span.innerHTML = projects[n].icon
-
     title.appendChild(span)
-    title.innerHTML = projects[n].title
+    title.append(`${projects[n].title}`)
+    dt_projects.appendChild(title)
+    item_project.appendChild(dt_projects)
+
+    const descricao = document.createElement("p")
+    descricao.innerHTML = projects[n].descricao
+    dt_projects.appendChild(descricao)
+
+    const container_links = document.createElement("div")
+    container_links.classList.add("links-projects")
+    const a1 = document.createElement("a")
+    a1.href = `${projects[n].acesso}`
+    a1.innerHTML = "🔗Acesse aqui"
+    a1.rel = "external"
+    a1.target = "_blank"
+    const a2 = document.createElement("a")
+    a2.href = `${projects[n].repositorio}`
+    a2.rel = "external"
+    a2.target = "_blank"
+    const icon = document.createElement("img")
+    icon.classList.add("icon-link")
+    icon.src = "./assets/img/github.png"
+    icon.alt = "github logo"
+    a2.appendChild(icon)
+    a2.append("Repositório")
+
+    container_links.appendChild(a1)
+    container_links.appendChild(a2)
+    item_project.appendChild(container_links)
 
 
+    container_projects.appendChild(item_project)
 
-    let html_item = `
-                <div class="item-project">
-                    <div class="description-title-projects">
-                        <div class="title-item-project">
-                            <span class="material-symbols-outlined">
-                                ${projects[n].icon}
-                            </span>
-                            ${projects[n].title}
-                        </div>
-                        <p>${projects[n].descricao}</p>
-                    </div>
-                    <div class="links-projects">
-                        <a href="${projects[n].acesso}">
-                            🔗Acesse aqui!
-                        </a>
-                        <a href="${projects[n].repositorio}">
-                            <img  class="icon-link" src="./assets/img/github.png" alt="github logo">
-                            Repositório
-                        </a>
-                    </div>
-                </div>
-`
 }
 
 
